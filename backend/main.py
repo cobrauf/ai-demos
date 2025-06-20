05a4251de2e655c37867027acc802fb78cab31ab
cobrauf <dyang922@gmail.com>
2025-06-20 10:34:29 -0700

    added test button function to call backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
+allowed_origins = [
+    "http://localhost:5173",
+    # frontend_url,
+    "*", # TODO: Restrict in production
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
---
+++ frontend/src/services/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const DEFAULT_API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8001" // When testing on the PC
+    : `http://${window.location.hostname}:8001`; // When testing from mobile (uses same hostname)
+
+const API_URL = import.meta.env.VITE_API_URL || DEFAULT_API_URL;
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+export const fetchRoot = async (): Promise<{ [key: string]: string }> => {
+  const response = await api.get("/");
+  return response.data;
+};
